94b29c08804a43c7ba904b63a73786b5b754fe7b
SergeyCherepanov <sergey@cherepanov.org.ua>
2018-11-12 10:37:18 +0400

    flock.rb

--- flock.rb
+++ flock.rb
@@ -3,7 +3,7 @@ require "formula"
 class Flock < Formula
   homepage "https://github.com/discoteq/flock"
   desc "flock implementation for macOS"
-  url "https://github.com/discoteq/flock/releases/download/v#{version}/flock-0.2.3.tar.xz"
+  url "https://github.com/discoteq/flock/releases/download/v0.2.3/flock-0.2.3.tar.xz"
   sha256 "3233658199683c807c21b0ef0fc32246e420f2a6e48f7044d2ccb763ff320c70"
   version "0.2.3"
 
