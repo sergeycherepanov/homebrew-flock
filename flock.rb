0e21bedf67a25af7c2a6b96ef8d752663d00bf46
SergeyCherepanov <sergey@cherepanov.org.ua>
2018-11-12 10:40:56 +0400

    .gitignore

--- flock.rb
+++ flock.rb
@@ -1,11 +1,11 @@
 require "formula"
 
 class Flock < Formula
-  homepage "https://github.com/discoteq/flock"
   desc "flock implementation for macOS"
+  homepage "https://github.com/discoteq/flock"
   url "https://github.com/discoteq/flock/releases/download/v0.2.3/flock-0.2.3.tar.xz"
-  sha256 "3233658199683c807c21b0ef0fc32246e420f2a6e48f7044d2ccb763ff320c70"
   version "0.2.3"
+  sha256 "3233658199683c807c21b0ef0fc32246e420f2a6e48f7044d2ccb763ff320c70"
 
   def install
     system "./configure", "--disable-debug",
