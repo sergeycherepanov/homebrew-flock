37a486f4797b1fe4edb06cc9995c2b16341480c9
SergeyCherepanov <sergey@cherepanov.org.ua>
2018-11-12 10:33:45 +0400

    flock.rb

--- flock.rb
+++ flock.rb
@@ -1,20 +1,21 @@
-require 'formula'
+require "formula"
 
 class Flock < Formula
-  homepage 'https://github.com/discoteq/flock'
-  version '0.2.3'
-  url "https://github.com/discoteq/flock/releases/download/v#{version}/flock-#{version}.tar.xz"
-  sha256 '3233658199683c807c21b0ef0fc32246e420f2a6e48f7044d2ccb763ff320c70'
+  homepage "https://github.com/discoteq/flock"
+  desc "flock implementation for macOS"
+  url "https://github.com/discoteq/flock/releases/download/v#{version}/flock-0.2.3.tar.xz"
+  sha256 "3233658199683c807c21b0ef0fc32246e420f2a6e48f7044d2ccb763ff320c70"
+  version "0.2.3"
 
   def install
-    system './configure', '--disable-debug',
-                          '--disable-dependency-tracking',
-                          '--disable-silent-rules',
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
                           "--prefix=#{prefix}"
-    system 'make', 'install'
+    system "make", "install"
   end
 
   test do
-    system "#{bin}/flock", 'tmpfile', 'true'
+    system "#{bin}/flock", "tmpfile", "true"
   end
 end
